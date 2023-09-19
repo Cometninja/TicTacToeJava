737898df4cb3f4bdd8036fe1f1b3edff48887325
Daniel Casey <daniel_mn_casey@hotmail.com>
2023-09-19 18:30:09 +0100

    initial commit

---
+++ src/main/java/com/hnd/tictactoe/TicTacToe.java
@@ -0,0 +1,18 @@
+/*
+    a simple tictactoe program that is displayed on the console
+
+*/
+
+package com.hnd.tictactoe;
+
+/**
+ *  Name: Daniel Casey
+ *  
+ * 
+ */
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
